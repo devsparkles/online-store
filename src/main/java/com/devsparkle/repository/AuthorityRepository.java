f216c8ae750c8fc620835c0e1c537a8668bc20bd
Xavier Maximin <xmaximin@gmail.com>
2020-07-01 10:37:39 +0100

    Initial version of store generated by JHipster-6.10.0

---
+++ src/main/java/com/devsparkle/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.devsparkle.repository;
+
+import com.devsparkle.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/devsparkle/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.devsparkle.service.dto;
